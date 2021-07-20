ec6dc569601f1b54303de0494951ea7d48caf618
Mukul <mukulkolpe45@gmail.com>
2021-07-21 00:39:52 +0530

    Landing page updated

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,11 +7,11 @@ const Landing = () => {
   return (
     <div className="landing-container">
       <div data-aos="fade-right" className="landing-left">
-        <h1 className="landing-header">Can you type...</h1>
+        <h1 className="landing-header">Lets improve typing...</h1>
         <div className="typewriter-container">
           <Typewriter
             options={{
-              strings: ["Fast?", "Correct?", "Quick?"],
+              strings: ["Speed", "Accuracy", "Technique"],
               autoStart: true,
               loop: true,
             }}
