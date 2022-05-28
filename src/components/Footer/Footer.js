14530dbe7f382d9034fd326fa601b131be3ff82b
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-05-29 02:09:53 +0530

    Footer enhanced

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,41 +4,45 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <div className="footer-header">👋Connect with me</div>
-      <a
-        href="https://www.linkedin.com/in/mukulkolpe/"
-        className="footer-link"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className="fa fa-linkedin"></i>
-      </a>
-      <a
-        href="https://twitter.com/MukulKolpe"
-        className="footer-link"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className="fa fa-twitter"></i>
-      </a>
+      <div className="footer-items">
+        <div className="footer-header">👋Connect with me </div>
+        <div className="footer-links">
+          <a
+            href="https://www.linkedin.com/in/mukulkolpe/"
+            className="footer-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa fa-linkedin"></i>
+          </a>
+          <a
+            href="https://twitter.com/MukulKolpe"
+            className="footer-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa fa-twitter"></i>
+          </a>
 
-      <a
-        href="https://github.com/MukulKolpe"
-        className="footer-link"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className="fa fa-github"></i>
-      </a>
+          <a
+            href="https://github.com/MukulKolpe"
+            className="footer-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa fa-github"></i>
+          </a>
 
-      <a
-        href="https://www.instagram.com/mukulkolpe/"
-        className="footer-link"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className="fa fa-instagram"></i>
-      </a>
+          <a
+            href="https://www.instagram.com/mukulkolpe/"
+            className="footer-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa fa-instagram"></i>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
